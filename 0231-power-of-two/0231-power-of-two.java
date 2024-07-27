c6655f5e40708617041111375e64485e4420c9d0
shubham <146070950+shubham98983@users.noreply.github.com>
2024-07-27 16:32:03 +0530

    Time: 0 ms (100%), Space: 40.6 MB (70.22%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==0 || n==Integer.MIN_VALUE) return false;
+        if((n&n-1)==0) return true;
+        return false;
+    }
+}
