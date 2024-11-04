648a2d274964c766326a4c719007faa749edc48e
shubham <146070950+shubham98983@users.noreply.github.com>
2024-11-04 22:37:27 +0530

    Time: 3 ms (40.26%), Space: 46.6 MB (87.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,35 +1,34 @@
 class Solution {
-    public static void dfs(ArrayList<ArrayList<Integer>> graph,int curr, boolean vis[]){
-        vis[curr]=true;
-        for(int i=0; i<graph.get(curr).size(); i++){
-            Integer e = graph.get(curr).get(i);
-            if(!vis[e]){
-                dfs(graph,e,vis);
+    public static void dfs(int node,boolean[] vis,ArrayList<ArrayList<Integer>> adj){
+        vis[node]=true;
+        for(int it:adj.get(node)){
+            if(!vis[it]){
+                dfs(it,vis,adj);
             }
         }
     }
     public int findCircleNum(int[][] isConnected) {
-        int count =0;
-     int n = isConnected.length;
-     ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-    for(int i=0; i<n; i++){
-        graph.add(new ArrayList<>());
-    }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(isConnected[i][j]==1 && i!=j){
-                graph.get(i).add(j);
-                graph.get(j).add(i);
+        int count=0;
+        int v = isConnected.length;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<v; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0; i<v; i++){
+            for(int j=0; j<v; j++){
+              if(isConnected[i][j]==1 && i!=j){
+                 adj.get(i).add(j);
+                 adj.get(j).add(i);
+              }
             }
         }
-    }
-     boolean vis[] = new boolean[n];
-    for(int i=0; i<n; i++){
-        if(vis[i]==false){
-            dfs(graph,i,vis);
-            count++;
+        boolean vis[] = new boolean[v];
+        for(int i=0; i<v; i++){
+            if(!vis[i]){
+                count++;
+                dfs(i,vis,adj);
+            }
         }
-    }
         return count;
     }
 }
