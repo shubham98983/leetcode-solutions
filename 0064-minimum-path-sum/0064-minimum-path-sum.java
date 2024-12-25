a79362a65e5946d138be070d649b7da2491c907c
shubham <146070950+shubham98983@users.noreply.github.com>
2024-12-25 21:11:48 +0530

    Time: 5 ms (31.77%), Space: 47.3 MB (79.6%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -11,9 +11,19 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int dp[][] = new int[m][n];
-        for(int[] row: dp){
-            Arrays.fill(row,-1);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+              if(i==0 && j==0) {
+                   dp[0][0]=grid[0][0];
+                   continue;
+              }
+              int left=(int)1e9;
+              int up= (int)1e9;
+              if(i>0) up = grid[i][j]+dp[i-1][j];
+              if(j>0) left = grid[i][j]+dp[i][j-1];
+              dp[i][j]=Math.min(up,left);
+            }
         }
-        return sum(m-1,n-1,grid,dp);
+        return dp[m-1][n-1];
     }
 }
