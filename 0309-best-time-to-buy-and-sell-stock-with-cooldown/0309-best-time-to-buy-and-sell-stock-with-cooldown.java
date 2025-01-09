4b2302801b2c90a0a15672c3553131abe290c89b
shubham <146070950+shubham98983@users.noreply.github.com>
2025-01-09 15:55:41 +0530

    Time: 1 ms (81.99%), Space: 42.2 MB (18.07%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,19 +1,23 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int dp[][] = new int[n+2][2];
+        int front2[] = new int[2];
+        int front1[] = new int[2];
+        int curr[] = new int[2];
         //base case already initialised with 0
         int profit=0;
         for(int i=n-1; i>=0; i--){
             for(int buy=1; buy>=0; buy--){   // what we take in col or our second changing parameter will be in inner loop
                 if(buy==0){
-                   profit = Math.max(-prices[i]+dp[i+1][1] , dp[i+1][0]);
+                   profit = Math.max(-prices[i]+front1[1] , front1[0]);
                 }else{
-                    profit = Math.max(prices[i]+dp[i+2][0] , dp[i+1][1]);
+                    profit = Math.max(prices[i]+front2[0] , front1[1]);
                 }
-                dp[i][buy]=profit;
+                curr[buy]=profit;
+             System.arraycopy(front1, 0, front2, 0, 2);
+            System.arraycopy(curr, 0, front1, 0, 2);
             }
         }
-       return dp[0][0];
+       return curr[0];
     }
 }
