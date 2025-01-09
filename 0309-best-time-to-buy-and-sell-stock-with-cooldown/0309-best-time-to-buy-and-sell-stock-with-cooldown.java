9ef93d3c70092e4bb238478985f97386efc6176c
shubham <146070950+shubham98983@users.noreply.github.com>
2025-01-09 15:26:14 +0530

    Time: 1 ms (81.99%), Space: 41.9 MB (50.82%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,20 +1,19 @@
 class Solution {
-        public int f(int i, int buy ,int[] prices,int[][] dp){
-       if(i>=prices.length) return 0;
-       int profit;
-       if(dp[i][buy]!=-1) return dp[i][buy];
-       if(buy==0){
-         profit = Math.max(-prices[i]+f(i+1,1,prices,dp) , 0+f(i+1,0,prices,dp));
-       }else{
-          profit = Math.max(prices[i] + f(i+2,0,prices,dp) , 0 + f(i+1,1,prices,dp));
-       }
-        return dp[i][buy] = profit;
-    }
     public int maxProfit(int[] prices) {
-        int dp[][] = new int[prices.length][2];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        int n = prices.length;
+        int dp[][] = new int[n+2][2];
+        //base case already initialised with 0
+        int profit=0;
+        for(int i=n-1; i>=0; i--){
+            for(int buy=1; buy>=0; buy--){   // what we take in col or our second changing parameter will be in inner loop
+                if(buy==0){
+                   profit = Math.max(-prices[i]+dp[i+1][1] , dp[i+1][0]);
+                }else{
+                    profit = Math.max(prices[i]+dp[i+2][0] , dp[i+1][1]);
+                }
+                dp[i][buy]=profit;
+            }
         }
-       return f(0,0,prices,dp);    
+       return dp[0][0];
     }
 }
