ccf362ae796717a98c7bc9869db2b5593cfe9a4f
shubham <146070950+shubham98983@users.noreply.github.com>
2025-01-11 00:49:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,27 +1,22 @@
 class Solution {
-    public int f(int i, int prev , int[] nums,int[][] dp){
-        if(i==nums.length) return 0;
-        int pick=0;
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        if(prev==-1 || nums[i]>nums[prev]){
-          pick = 1+f(i+1,i,nums,dp);
-        }
-        int notPick = f(i+1,prev,nums,dp);
-        return dp[i][prev+1] = Math.max(pick,notPick);
-    }
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int dp[][] = new int[n+1][n+1];
-        for(int i=n-1; i>=0; i--){
-            for(int prev=i-1; prev>=-1; prev--){    
-               int pick=0;
-               if(prev==-1 || nums[i]>nums[prev]){
-                   pick = 1 + dp[i+1][i+1];
-               }
-               int notPick = dp[i+1][prev+1];
-               dp[i][prev+1] = Math.max(pick,notPick);
+        int length=1;
+        int maxLength=0;
+        for(int i=0; i<n-1; i++){
+            int num = nums[i];
+            for(int j=i+1; j<n; j++){
+                if(nums[j]>num){
+                    length++;
+                    num=nums[j];
+                    maxLength=Math.max(length,maxLength);
+                }
             }
+            length=1;
         }
-        return dp[0][0];
+        return maxLength;
     }
 }
+
+
+//this type of solution fail on subsequence here we can't explore all subsequence by skipping largest in middle if here comes a large number in middlle it will pick and will make it part of subsequnence like in [0,1,0,3,2,3] correct susbsequnce[0,1,2,3] acc to our code [0,1,3] it will pick first 3 and 2 will not be considered as part of subsequence.
